08faddd60ab10a564db28ac36f04f5f1fdf6db25
zababurinph <raccoontd@mail.ru>
2022-09-16 02:08:57 +0300

    auadio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-let m = [
-    main = document.getElementById('main'),
-    l1 = document.getElementById('l1'),
-    l2 = document.getElementById('l2'),
-    l3 = document.getElementById('l3'),
-    l4 = document.getElementById('l4')
-]
-
 function changePage(idOld, idNew) {
     document.getElementById(idOld).style.display = 'none'
     document.getElementById(idNew).style.display = 'block'
-}
-
-function closeAll(id) {
-    m.forEach((e) => e.style.display = 'none')
-    document.getElementById(id).style.display = 'block'
 }
