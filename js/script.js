2b7e75f004d1b6e77a863842ed21f8a54ecbbb4d
zababurinph <raccoontd@mail.ru>
2022-09-15 19:02:38 +0300

    pagesInOne

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-window.addEventListener('resize', (e) => {
-    if (document.documentElement.clientWidth > 600) document.getElementById('mobileMenu').classList.remove('active')
-}, true);
+let m = [
+    main = document.getElementById('main'),
+    l1 = document.getElementById('l1'),
+    l2 = document.getElementById('l2'),
+    l3 = document.getElementById('l3'),
+    l4 = document.getElementById('l4')
+]
 
-function openMenu() {
-    document.getElementById('mobileMenu').classList.toggle('active')
+function changePage(idOld, idNew) {
+    document.getElementById(idOld).style.display = 'none'
+    document.getElementById(idNew).style.display = 'block'
+}
+
+function closeAll(id) {
+    m.forEach((e) => e.style.display = 'none')
+    document.getElementById(id).style.display = 'block'
 }
